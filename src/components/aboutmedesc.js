b2915d652a14b59f061a8834b7dfd8b66d482ff9
Andrew Bratcher <abratcher1066@gmail.com>
2020-08-20 21:45:47 -0400

    Finalizing layouts

---
+++ src/components/aboutmedesc.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Grid, Cell, Button } from 'react-mdl';
+
+class Aboutmedesc extends Component {
+    render() {
+        return(
+            <div className="about-me" >
+                <p>Hey, my name's Andrew. 
+                <p>I'm a junior web developer with a passion for uncovering my own ignorance, one tiny step at a time.</p> 
+                </p>
+                <p>I also have a dog and I paint miniatures as a side hustle. </p>
+                
+                <p>Ex-cool history teacher. I read too much science fiction and love to cook. </p>
+            </div>
+        )
+    }
+}
+
+export default Aboutmedesc;
